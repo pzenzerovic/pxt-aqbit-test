bd1a88b99e921f46cd93ab88ed9dca98add1cf91
Ivan Bosnić <bosnivan@gmail.com>
2020-03-15 17:27:15 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+/**
+ * AQ:bit commands.
+ */
 enum HttpMethod {
     GET,
     POST,
@@ -9,19 +12,72 @@ enum HttpMethod {
     CONNECT,
     TRACE
 }
-
-enum Newline {
-    CRLF,
-    LF,
-    CR
+enum BME280_I2C_ADDRESS {
+    //% block="0x76"
+    ADDR_0x76 = 0x76,
+    //% block="0x77"
+    ADDR_0x77 = 0x77
 }
+enum BME280_P {
+    //% block="Pa"
+    Pa = 0,
+    //% block="hPa"
+    hPa = 1
+}
+// % color=#4698CB weight=90 icon="\uf2c8"
+// block="AQ:bit"
+namespace AQbit {
 
+    function serialToPMS(): void {
+        serial.redirect(
+            SerialPin.P1,
+            SerialPin.P0,
+            BaudRate.BaudRate9600
+        )
+    }
 
-/**
- * WiFi:bit commands.
- */
-//% color=#3452C3 weight=90 icon="\uf1eb" block="WiFi:bit"
-namespace WiFiBit {
+    /**
+     * Put PMS in passive mode.
+     */
+    //% weight=100
+    //% blockId="aqb_pms_pasive" block="put PMS in passive mode"
+    export function putPMSInPassiveMode(): void {
+        serialToPMS()
+        serial.setRxBufferSize(8)
+        let bufr = pins.createBuffer(7);
+        bufr.setNumber(NumberFormat.UInt8LE, 0, 66);
+        bufr.setNumber(NumberFormat.UInt8LE, 1, 77);
+        bufr.setNumber(NumberFormat.UInt8LE, 2, 225);
+        bufr.setNumber(NumberFormat.UInt8LE, 3, 0);
+        bufr.setNumber(NumberFormat.UInt8LE, 4, 0);
+        bufr.setNumber(NumberFormat.UInt8LE, 5, 1);
+        bufr.setNumber(NumberFormat.UInt8LE, 6, 112);
+        serial.writeBuffer(bufr)
+        basic.pause(500)
+    }
+
+    /**
+     * Read PMS value.
+     */
+    //% weight=99
+    //% blockId="aqb_read_pms" block="read PMS 2.5"
+    export function readPMS(): number {
+        serialToPMS()
+        serial.setRxBufferSize(32)
+        let bufr2 = pins.createBuffer(7);
+        bufr2.setNumber(NumberFormat.UInt8LE, 0, 66);
+        bufr2.setNumber(NumberFormat.UInt8LE, 1, 77);
+        bufr2.setNumber(NumberFormat.UInt8LE, 2, 226);
+        bufr2.setNumber(NumberFormat.UInt8LE, 3, 0);
+        bufr2.setNumber(NumberFormat.UInt8LE, 4, 0);
+        bufr2.setNumber(NumberFormat.UInt8LE, 5, 1);
+        bufr2.setNumber(NumberFormat.UInt8LE, 6, 113);
+        serial.writeBuffer(bufr2)
+        basic.pause(1000)
+        return serial.readBuffer(32)[13]
+    }
+
+    let pauseBaseValue: number = 100
 
     function writeToSerial(data: string, waitTime: number): void {
         serial.writeString(data + "\u000D" + "\u000A")
@@ -30,80 +86,7 @@ namespace WiFiBit {
         }
     }
 
-    let pauseBaseValue: number = 1000
-
-    /**
-     * Change HTTP method wait period.
-     * @param newPauseBaseValue Base value, eg: 1000
-     */
-    //% weight=1
-    export function changeHttpMethodWaitPeriod(newPauseBaseValue: number): void {
-        pauseBaseValue = newPauseBaseValue
-    }
-
-    /**
-     * Make a serial connection between micro:bit and WiFi:bit.
-     */
-    //% weight=100
-    //% blockId="wfb_connect" block="connect to WiFi:bit"
-    export function connectToWiFiBit(): void {
-        serial.redirect(
-            SerialPin.P16,
-            SerialPin.P8,
-            BaudRate.BaudRate115200
-        )
-        basic.pause(100)
-        // Restart module:
-        writeToSerial("AT+RST", 2000)
-        // WIFI mode = Station mode (client):
-        writeToSerial("AT+CWMODE=1", 5000)
-    }
-
-    /**
-     * Connect to WiFi network.
-     * @param ssid SSID, eg: "SSID"
-     * @param key Key, eg: "key"
-     */
-    //% weight=99
-    //% blockId="wfb_wifi_on" block="connect to WiFi network %ssid, %key"
-    export function connectToWiFiNetwork(ssid: string, key: string): void {
-        // Connect to AP:
-        writeToSerial("AT+CWJAP=\"" + ssid + "\",\"" + key + "\"", 6000)
-    }
-
-    /**
-     * Disconnect from WiFi network.
-     */
-    //% weight=98
-    //% blockId="wfb_wifi_off" block="disconnect from WiFi network"
-    export function disconnectFromWiFiNetwork(): void {
-        // Disconnect from AP:
-        writeToSerial("AT+CWQAP", 6000)
-    }
-
-    /**
-     * Execute AT command.
-     * @param command AT command, eg: "AT"
-     * @param waitTime Wait time after execution, eg: 1000
-     */
-    //% weight=97
-    //% blockId="wfb_at" block="execute AT command %command and then wait %waitTime ms"
-    export function executeAtCommand(command: string, waitTime: number): void {
-        writeToSerial(command, waitTime)
-    }
-
-    /**
-     * Execute HTTP method.
-     * @param method HTTP method, eg: HttpMethod.GET
-     * @param host Host, eg: "google.com"
-     * @param port Port, eg: 80
-     * @param urlPath Path, eg: "/search?q=something"
-     * @param headers Headers
-     * @param body Body
-     */
-    //% weight=96
-    //% blockId="wfb_http" block="execute HTTP method %method|host: %host|port: %port|path: %urlPath||headers: %headers|body: %body"
-    export function executeHttpMethod(method: HttpMethod, host: string, port: number, urlPath: string, headers?: string, body?: string): void {
+    function executeHttpMethod(method: HttpMethod, host: string, port: number, urlPath: string, headers?: string, body?: string): void {
         let myMethod: string
         switch (method) {
             case HttpMethod.GET: myMethod = "GET"; break;
@@ -116,7 +99,6 @@ namespace WiFiBit {
             case HttpMethod.CONNECT: myMethod = "CONNECT"; break;
             case HttpMethod.TRACE: myMethod = "TRACE";
         }
-        // Establish TCP connection:
         let data: string = "AT+CIPSTART=\"TCP\",\"" + host + "\"," + port
         writeToSerial(data, pauseBaseValue * 6)
         data = myMethod + " " + urlPath + " HTTP/1.1" + "\u000D" + "\u000A"
@@ -128,11 +110,32 @@ namespace WiFiBit {
             data += "\u000D" + "\u000A" + body + "\u000D" + "\u000A"
         }
         data += "\u000D" + "\u000A"
-        // Send data:
         writeToSerial("AT+CIPSEND=" + (data.length + 2), pauseBaseValue * 3)
         writeToSerial(data, pauseBaseValue * 6)
-        // Close TCP connection:
         writeToSerial("AT+CIPCLOSE", pauseBaseValue * 3)
+    }
+
+    function connectToWiFiBit(): void {
+        serial.redirect(
+            SerialPin.P16,
+            SerialPin.P8,
+            BaudRate.BaudRate115200
+        )
+        basic.pause(100)
+    }
+
+    /**
+     * Connect to WiFi network.
+     * @param ssid SSID, eg: "SSID"
+     * @param key Key, eg: "key"
+     */
+    //% weight=98
+    //% blockId="aqb_wifi_on" block="connect to WiFi network %ssid, %key"
+    export function connectToWiFiNetwork(ssid: string, key: string): void {
+        connectToWiFiBit()
+        writeToSerial("AT+RST", 2000)
+        writeToSerial("AT+CWMODE=1", 5000)
+        writeToSerial("AT+CWJAP=\"" + ssid + "\",\"" + key + "\"", 6000)
     }
 
     /**
@@ -141,9 +144,10 @@ namespace WiFiBit {
      * @param pin Pin, eg: "A0"
      * @param auth_token Token, eg: "14dabda3551b4dd5ab46464af582f7d2"
      */
-    //% weight=95
-    //% blockId="wfb_blynk_write" block="Blynk: write %value to %pin, token is %auth_token"
+    //% weight=97
+    //% blockId="aqb_blynk_write" block="Blynk write %value to %pin, token is %auth_token"
     export function writePinValue(value: string, pin: string, auth_token: string): void {
+        connectToWiFiBit()
         executeHttpMethod(
             HttpMethod.GET,
             "blynk-cloud.com",
@@ -152,38 +156,128 @@ namespace WiFiBit {
         )
     }
 
-    /**
-     * Read Blynk pin value.
-     * @param pin Pin, eg: "A0"
-     * @param auth_token Token, eg: "14dabda3551b4dd5ab46464af582f7d2"
-     */
-    //% weight=94
-    //% blockId="wfb_blynk_read" block="Blynk: read %pin, token is %auth_token"
-    export function readPinValue(pin: string, auth_token: string): string {
-        executeAtCommand("ATE0", 1000)
-        let response: string
-        serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
-            response += serial.readString()
-        })
-        executeHttpMethod(
-            HttpMethod.GET,
-            "blynk-cloud.com",
-            80,
-            "/" + auth_token + "/get/" + pin
-        )
-        let value: string = response.substr(response.indexOf("[") + 2, response.indexOf("]") - response.indexOf("[") - 3)
-        response = null
-        serial.onDataReceived(serial.delimiters(Delimiters.NewLine), () => { })
-        return value
+    let BME280_I2C_ADDR = BME280_I2C_ADDRESS.ADDR_0x76
+
+    function setreg(reg: number, dat: number): void {
+        let buf = pins.createBuffer(2);
+        buf[0] = reg;
+        buf[1] = dat;
+        pins.i2cWriteBuffer(BME280_I2C_ADDR, buf);
+    }
+
+    function getreg(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.UInt8BE);
+    }
+
+    function getInt8LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.Int8LE);
+    }
+
+    function getUInt16LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.UInt16LE);
+    }
+
+    function getInt16LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.Int16LE);
+    }
+
+    let dig_T1 = getUInt16LE(0x88)
+    let dig_T2 = getInt16LE(0x8A)
+    let dig_T3 = getInt16LE(0x8C)
+    let dig_P1 = getUInt16LE(0x8E)
+    let dig_P2 = getInt16LE(0x90)
+    let dig_P3 = getInt16LE(0x92)
+    let dig_P4 = getInt16LE(0x94)
+    let dig_P5 = getInt16LE(0x96)
+    let dig_P6 = getInt16LE(0x98)
+    let dig_P7 = getInt16LE(0x9A)
+    let dig_P8 = getInt16LE(0x9C)
+    let dig_P9 = getInt16LE(0x9E)
+    let dig_H1 = getreg(0xA1)
+    let dig_H2 = getInt16LE(0xE1)
+    let dig_H3 = getreg(0xE3)
+    let a = getreg(0xE5)
+    let dig_H4 = (getreg(0xE4) << 4) + (a % 16)
+    let dig_H5 = (getreg(0xE6) << 4) + (a >> 4)
+    let dig_H6 = getInt8LE(0xE7)
+    setreg(0xF2, 0x04)
+    setreg(0xF4, 0x2F)
+    setreg(0xF5, 0x0C)
+    let T = 0
+    let P = 0
+    let H = 0
+
+    function get(): void {
+        let adc_T = (getreg(0xFA) << 12) + (getreg(0xFB) << 4) + (getreg(0xFC) >> 4)
+        let var1 = (((adc_T >> 3) - (dig_T1 << 1)) * dig_T2) >> 11
+        let var2 = (((((adc_T >> 4) - dig_T1) * ((adc_T >> 4) - dig_T1)) >> 12) * dig_T3) >> 14
+        let t = var1 + var2
+        T = Math.idiv((t * 5 + 128) >> 8, 100)
+        var1 = (t >> 1) - 64000
+        var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * dig_P6
+        var2 = var2 + ((var1 * dig_P5) << 1)
+        var2 = (var2 >> 2) + (dig_P4 << 16)
+        var1 = (((dig_P3 * ((var1 >> 2) * (var1 >> 2)) >> 13) >> 3) + (((dig_P2) * var1) >> 1)) >> 18
+        var1 = ((32768 + var1) * dig_P1) >> 15
+        if (var1 == 0)
+            return;
+        let adc_P = (getreg(0xF7) << 12) + (getreg(0xF8) << 4) + (getreg(0xF9) >> 4)
+        let _p = ((1048576 - adc_P) - (var2 >> 12)) * 3125
+        _p = Math.idiv(_p, var1) * 2;
+        var1 = (dig_P9 * (((_p >> 3) * (_p >> 3)) >> 13)) >> 12
+        var2 = (((_p >> 2)) * dig_P8) >> 13
+        P = _p + ((var1 + var2 + dig_P7) >> 4)
+        let adc_H = (getreg(0xFD) << 8) + getreg(0xFE)
+        var1 = t - 76800
+        var2 = (((adc_H << 14) - (dig_H4 << 20) - (dig_H5 * var1)) + 16384) >> 15
+        var1 = var2 * (((((((var1 * dig_H6) >> 10) * (((var1 * dig_H3) >> 11) + 32768)) >> 10) + 2097152) * dig_H2 + 8192) >> 14)
+        var2 = var1 - (((((var1 >> 15) * (var1 >> 15)) >> 7) * dig_H1) >> 4)
+        if (var2 < 0) var2 = 0
+        if (var2 > 419430400) var2 = 419430400
+        H = (var2 >> 12) >> 10
+    }
+
+    function PowerOn() {
+        setreg(0xF4, 0x2F)
+    }
+
+    function PowerOff() {
+        setreg(0xF4, 0)
     }
 
     /**
-     * Line separator. It's used when headers or body are multiline.
+     * Read BME temperature.
      */
-    //% weight=93
-    //% blockId="wfb_crlf" block="CRLF"
-    export function newline(): string {
-        return "\u000D" + "\u000A"
+    //% weight=96
+    //% blockId="aqb_read_temperature" block="read BME temperature"
+    export function readBMETemperature(): number {
+        get()
+        return T
+    }
+
+    /**
+     * Read humidity.
+     */
+    //% weight=95
+    //% blockId="aqb_read_humidity" block="read humidity"
+    export function readHumidity(): number {
+        get()
+        return H
+    }
+
+    /**
+     * Read pressure.
+     */
+    //% weight=94
+    //% blockId="aqb_read_pressure" block="read pressure %u"
+    export function readPressure(u: BME280_P): number {
+        get()
+        if (u == BME280_P.Pa) return P
+        else return Math.idiv(P, 100)
     }
 
 }
